9d2810b10a5b062bd34604101f25bb4dde9d0834
Daniel Diseroad <dsdiser@gmail.com>
2026-03-27 21:47:56 -0400

    Add game state types

---
+++ client/types/gameState.ts
@@ -0,0 +1,7 @@
+const enum GameState {
+  WaitingForPlayers = 'WaitingForPlayers',
+  DealingCards = 'DealingCards',
+  PlayingTricks = 'PlayingTricks',
+  RoundSummary = 'RoundSummary',
+  GameEnded = 'GameEnded',
+}
